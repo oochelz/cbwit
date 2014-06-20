e4c45336801312b2c66de76dac9666691e48d7e5
Chelsea Youmans <chelsea.youmans@careerbuilder.com>
2014-06-20 09:31:26 -0400

    tweaks for lunch bunches

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,28 +3,31 @@ class HomeController < ApplicationController
     @lunch_bunches = load_lunch_bunches
   end
 
-
-
   def join_group
-    lunchName = params[:lunchName]
-    group_number = 1 + rand(6)
+    @lunch_name = params[:lunchName]
+    @group_number = 1 + rand(6)
     @bunches = load_lunch_bunches
-    group = @bunches[group_number.to_s]
-    group['people'] << lunchName
-    @bunches[group_number] = group
+    group = @bunches[@group_number.to_s]
+    group['people'] << @lunch_name
+    @bunches[@group_number] = group
     $redis.set('bunches', @bunches.to_json)
+  end
+
+  def clear_groups
+    $redis.del 'bunches'
   end
 
   private
 
   def load_lunch_bunches
-    json_bunches = $redis.get('bunches')
+    json_bunches = $redis.get 'bunches'
     if json_bunches
       bunches = JSON.parse json_bunches
     else
       bunches = Hash.new
       6.times { |i| bunches[i + 1] = Group.new(i + 1) }
     end
-    bunches
+    $redis.set('bunches', bunches.to_json)
+    JSON.parse($redis.get('bunches'))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'join_group', to: 'home#join_group'
+  get 'clear_groups', to: 'home#clear_groups'
 end
